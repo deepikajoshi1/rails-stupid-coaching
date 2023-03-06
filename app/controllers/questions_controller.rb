5c6db58cf5f2156f582e1070022d8a9bb23c97cb
deepika joshi <upreti.deepika@gmail.com>
2023-03-06 11:27:49 +0800

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,36 +1,6 @@
 class QuestionsController < ApplicationController
-  def ask; end
-  def answer
-    @question = params[:question]
-    @answer = reaction(@question)
-  end
-  def reaction(question)
-
-
-    return %(Great!) if question.downcase == %(i am going to work) && !question.nil?
-
-    text1 = %(Silly question, get dressed and go to work!)
-    text2 = %(I don't care, get dressed and go to work!)
-    question.end_with?('?') ? text1 : text2
-  end
-end
-
-------------------------------------------------------------------------------------------
-
-class QuestionsController < ApplicationController
   def ask
   end
-
   def answer
-    @question = params[:question]
-    if @question.blank?
-      @answer = "I can't hear you!"
-    elsif @question =~ /i am going to work/i
-      @answer = "Great!"
-    elsif @question.ends_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
   end
 end
